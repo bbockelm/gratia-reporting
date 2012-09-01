1fc4d41460329bf0c219123282f3199f9ee362a2
brian <brian@1fda6f16-4416-0410-bfd6-867a04880151>
2012-09-01 16:26:56 +0000

    Version bump for support for jobs report.
    git-svn-id: svn://t2.unl.edu/brian/GratiaReporting@4626 1fda6f16-4416-0410-bfd6-867a04880151

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name="GratiaReporting",
-  version="0.3.9",
+  version="0.4.0",
   author="Brian Bockelman",
   author_email="bbockelm@cse.unl.edu",
   description="Gratia reporting package.",
